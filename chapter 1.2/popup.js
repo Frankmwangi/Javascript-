f9b37e913098936bac4e0c21abcae7a665d449c5
Frank Mwangi <frankmwangi007@gmail.com>
2020-04-06 21:25:23 +0300

    ARRAYS

--- chapter 1.2/popup.js
+++ chapter 1.2/popup.js
@@ -10,7 +10,7 @@ close.addEventListener('click',() =>{
     popup.addEventListener('click',() =>{
         popup.style.display = 'none';
         });*/
-        const form = document.querySelector('.signup-form');
+      /*  const form = document.querySelector('.signup-form');
       //  const username = document.querySelector('#username');
       const feedback = document.querySelector('.feedback');
       const usernamePattern = /^[a-zA-z]{6,12}$/
@@ -44,6 +44,30 @@ close.addEventListener('click',() =>{
             console.log('regex test failed');
         }*/
       // let result = username.search(pattern);
-       // console.log(result);
+       // console.log(result);*/
+       //const scores = [10,30,15,25,50,40,5];
 
+       // filteredscores = scores.filter((scores)=>{
+        //   return scores > 20;
+      // });
+
+      // console.log(filteredscores);
+     /* const users = [
+{name: 'shaun', premium:true},
+{name: 'yoshi', premium:false},
+{name: 'mario', premium:false},
+{name: 'chun-li', premium:true},
+
+      ];
+      const premiumusers = users.filter(users => users.premium);
+      console.log(premiumusers);*/
+      // map method
+     /* const prices = [10,5,0,40,60,10,20,70];
+      const salePrice = prices.map((price) =>{
+          return price /2;
+
+      });
+      console.log(salePrice);*/
+      const score =[10,20,60,40,70,90,30];
+      
         
