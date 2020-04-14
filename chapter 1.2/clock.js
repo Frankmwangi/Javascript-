9fe891511343b5aab43073c0bd0863949f901892
Frank Mwangi <frankmwangi007@gmail.com>
2020-04-14 13:49:16 +0300

    JSON

--- chapter 1.2/clock.js
+++ chapter 1.2/clock.js
@@ -1,21 +1,11 @@
-
-
-const clock = document.querySelector('.clock');
-const tick = () => {
 const now = new Date();
-const h = now.getHours();
-const m= now.getMinutes();
-const s = now.getSeconds();
 
-const html = `
-<span>${h}</span>:
-<span>${m}</span>:
-<span>${s}</span>
+//console.log(dateFns.isToday(now));
+console.log(dateFns.format(now, 'YYYY'));
+console.log(dateFns.format(now, 'MMMM'));
+console.log(dateFns.format(now, 'dddd'));
+console.log(dateFns.format(now, 'dddd,Do,MMMM,YYYY'));
 
-`;
-clock.innerHTML = html;
-
-};
-
-
-setInterval(tick,1000);
+//comparing dates
+const before = new Date('February 1 2019 12:00:00');
+console.log(dateFns.distanceInWords(now,before,{addSuffix: true}));
